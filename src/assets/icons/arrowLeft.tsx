2be5c235a686b89735836668e003d29929cee56a
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-06-26 03:48:40 +0900

    add SVG file

---
+++ src/assets/icons/arrowLeft.tsx
@@ -0,0 +1,24 @@
+const ArrowLeft = () => {
+  return (
+    <div>
+      <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M15.5833 9.20834H2.125"
+          stroke="#5F6368"
+          stroke-width="2.2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M8.5 15.5833L1.41666 8.85417L8.5 2.125"
+          stroke="#5F6368"
+          stroke-width="2.2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default ArrowLeft;
---
+++ src/assets/icons/arrowRight.tsx
@@ -0,0 +1,25 @@
+const ArrowRight = () => {
+  return (
+    <div>
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_1011_716)">
+          <path d="M2 11L21 11" stroke="#F7F7F7" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round" />
+          <path
+            d="M12 2L22 11.5L12 21"
+            stroke="#F7F7F7"
+            stroke-width="2.2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_1011_716">
+            <rect width="24" height="24" fill="white" transform="translate(24 24) rotate(-180)" />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  );
+};
+
+export default ArrowRight;
