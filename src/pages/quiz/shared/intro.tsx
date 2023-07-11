c1b4a41af0c38509bdf0b8d4e9ce0c87c7630526
hohyeon <hohyeon@tossinvest.com>
2023-07-11 11:51:23 +0900

    fix: 빌드에러 주석처리

--- src/pages/quiz/shared/intro.tsx
+++ src/pages/quiz/shared/intro.tsx
@@ -1,19 +1,15 @@
-import { useQueryParam } from '@/hooks/useQueryParam';
-import { assert } from '@toss/assert';
+// import { useQueryParam } from '@/hooks/useQueryParam';
+// import { assert } from '@toss/assert';
 
 // 공유받은 ver 진입 페이지
 const ShareQuizIntroPage = () => {
-  const userId = useQueryParam<number>('user-id');
+  // const userId = useQueryParam<number>('user-id');
 
-  console.log(userId);
+  // console.log(userId);
 
-  assert(userId !== undefined, 'userId is required');
+  // assert(userId !== undefined, 'userId is required');
 
-  return (
-    <main>
-      <h1>{convertUserIdToUserName(userId)}님의 밸런스게임</h1>
-    </main>
-  );
+  return <main>{/* <h1>{convertUserIdToUserName(userId)}님의 밸런스게임</h1> */}</main>;
 };
 
 export default ShareQuizIntroPage;
